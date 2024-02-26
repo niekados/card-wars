45099191a38a64c3d3fd0c4a6ac32c8961ea000c
niekados <tiny.post@yahoo.com>
2024-02-26 17:26:15 +0000

    Add eventListeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 // List of DOM elements
 let movesCounter = document.getElementById('moves-counter');
 let displayComputerCard = document.getElementById('computer-card-front');
@@ -14,3 +12,38 @@ let deckSize = 'small';
 let playerDeck;
 let computerDeck;
 let warDeck = [];
+
+// Get all buttons and add eventlisteners
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => button.addEventListener('click', (event) => {
+    let buttonType = button.getAttribute('data-type');
+
+    // Removes "focus" from all buttons
+    button.blur()
+
+    if (buttonType === 'reset') {
+        resetGame()
+        startGame()
+    }
+    if (buttonType === 'small-deck') {
+        button.setAttribute('data-type', 'large-deck');
+        button.innerHTML = '<i class="fa-solid fa-layer-group"></i> Large';
+        deckSize = 'full';
+        resetGame()
+        startGame()
+    }
+    if (buttonType === 'large-deck') {
+        button.setAttribute('data-type', 'small-deck');
+        button.innerHTML = '<i class="fa-solid fa-layer-group"></i> Small';
+        deckSize = 'small';
+        resetGame()
+        startGame()
+    }
+    if (buttonType === "info") {
+        gameRules()
+    }
+    if (buttonType === 'play') {
+        playGame()
+    }
+}))
